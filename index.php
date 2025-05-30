ab43c90b3e365aa754f1a60c18a87d8d2a8a71fe
Denismaka <makadenis370@gmail.com>
2025-05-30 13:20:23 +0100

    Mise en place du formulaire

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$title = 'GuestBook';
+require("layouts/header.php")
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GuestBook</title>
-</head>
+<div class="container">
+    <h1>GuestBook</h1>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" name="username" placeholder="Enter your name" class="form-control">
+        </div>
+        <div class="form-group">
+            <textarea name="message" placeholder="Enter your message" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 
-<body>
 
-</body>
 
-</html>
+
+
+
+<?php require("layouts/footer.php") ?>
