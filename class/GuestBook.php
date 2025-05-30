ef78128911f39164b375c4ce50e7a33d636b42fb
Denismaka <makadenis370@gmail.com>
2025-05-30 21:02:24 +0100

    correction de la logique

--- class/GuestBook.php
+++ class/GuestBook.php
@@ -36,4 +36,15 @@ class GuestBook
     {
         file_put_contents($this->file, $message->toJSON() . PHP_EOL, FILE_APPEND);
     }
+
+    public function getMessages(): array
+    {
+        $messages = [];
+        $content = trim(file_get_contents($this->file));
+        $lines = explode(PHP_EOL, $content);
+        foreach ($lines as $line) {
+            $messages[] = Message::fromJSON($line);
+        }
+        return array_reverse($messages);
+    }
 }
