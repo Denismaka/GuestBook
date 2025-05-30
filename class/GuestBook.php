c90539704d248bceae80be696ecaa25b6c60d97b
Denismaka <makadenis370@gmail.com>
2025-05-30 15:11:32 +0100

    Ajout des commentaires explicatifs

--- class/GuestBook.php
+++ class/GuestBook.php
@@ -1,21 +1,37 @@
 <?php
 require_once "class/message.php";
+
+/**
+ * Classe GuestBook
+ * Gère le stockage et la récupération des messages du livre d'or
+ */
 class GuestBook
 {
+    // Chemin du fichier de stockage des messages
     private string $file;
 
+    /**
+     * Constructeur de la classe GuestBook
+     * @param string $file Chemin du fichier de stockage
+     */
     public function __construct(string $file)
     {
+        // Création du répertoire si nécessaire
         $directory = dirname($file);
         if (!is_dir($directory)) {
             mkdir($directory, 0777, true);
         }
+        // Création du fichier s'il n'existe pas
         if (!file_exists($file)) {
             touch($file);
         }
         $this->file = $file;
     }
-    // ajouter un message
+
+    /**
+     * Ajoute un message au livre d'or
+     * @param Message $message Le message à ajouter
+     */
     public function addMessage(Message $message): void
     {
         file_put_contents($this->file, $message->toJSON() . PHP_EOL, FILE_APPEND);
